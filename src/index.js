9cd16daff74b953213cf270f765dc1f280219141
magervalentine <magervalentine@gmail.com>
2020-02-19 22:05:31 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-  return 0; 
+  return array == undefined || array.length == 0 ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array == undefined || array.length == 0 ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array == undefined || array.length == 0 ? 0 : array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length;
 }
